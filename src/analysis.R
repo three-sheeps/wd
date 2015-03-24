745d6d9f1c3d9270cbed0e43240325cacade9e75
Your Collegue <yourcollegue@uni.com>
2015-03-24 11:00:39 +0100

    first path of analysis

---
+++ src/analysis.R
@@ -0,0 +1,14 @@
+# get cleaned data
+source("lib/get_clean_data.R")
+mammal <- get_clean_data()
+
+str(mammal)
+
+# correlation between body mass and field metabolic rate dependend on whether
+# the mammal eats other mammals
+
+library(data.table)
+mammalt <- as.data.table(mammal)
+mammalt[,
+        cor(BMR, Mass.BMR, use = "com"),
+        by = MammalEater]
